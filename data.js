0092b1892df3bd9de225c7e1144c8bdbf92a221d
vinhanvo <vinhan1404@gmail.com>
2026-01-13 13:57:08 +0700

    Cleaned code

---
+++ data.js
@@ -0,0 +1,43 @@
+const langData = {
+    header: {
+        logoHeader: "Shipway",
+        userName: "User name",
+        logout: "Đăng xuất"
+    },
+    title: "Chi tiết đơn hàng",
+    status1: "Tìm tài xế",
+    status2: "Đang vận chuyển",
+    status3: "Hoàn thành",
+    status4: "Đã huỷ",
+    back: "Quay lại",
+    labels: {
+        orderId: "Mã đơn hàng",
+        pickup: "Điểm nhận hàng",
+        delivery: "Điểm trả hàng",
+        type: "Loại hàng",
+        size: "Kích thước",
+        weight: "Trọng lượng",
+        driver: "Tài xế",
+        driverPhone: "SĐT tài xế",
+        cost: "Tổng tiền",
+        date: "Ngày đặt",
+        images: "Ảnh hàng hoá"
+    },
+    values: {
+        orderId: "ABC001",
+        pickup: "Nguyễn Lương Bằng, Phường Liên Chiểu, Thành phố Đà Nẵng",
+        delivery: "Khu nhà xưởng số 1, đường số 9, Khu Công nghệ cao Đà Nẵng, Xã Hòa Liên, Huyện Hòa Vang, Thành phố Đà Nẵng",
+        type: "Tên loại hàng",
+        size: "dài x rộng x cao",
+        weight: "Chủ hàng tự ước tính",
+        driver: "Chưa có",
+        driverPhone: "Chưa có",
+        cost: "Chủ hàng tự ước tính",
+        date: "dd/mm/yyyy",
+        productImage: [
+            "img/box.jpg",
+            "img/box.jpg",
+            "img/box.jpg"
+        ]
+    }
+};
