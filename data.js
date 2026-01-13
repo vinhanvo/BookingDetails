8ec9f81d2735ae668351e89332cacb05a4b8bfc7
vinhanvo <vinhan1404@gmail.com>
2026-01-13 15:49:27 +0700

    Updated full

--- data.js
+++ data.js
@@ -2,14 +2,25 @@ const langData = {
     header: {
         logoHeader: "Shipway",
         userName: "User name",
-        logout: "Đăng xuất"
+        userLink: "https://dut.udn.vn/",
+        wallet: "Mở ví",
+        walletLink: "http://google.com/",
+        logout: "Đăng xuất",
+        logoutLink: "https://facebook.com/",
+        title: "Chi tiết đơn hàng",
+        statusTexts: {
+            1: "Tìm tài xế",
+            2: "Đang vận chuyển",
+            3: "Hoàn thành",
+            4: "Đã huỷ"
+        },
+        back: "Quay lại",
+        backLink: "http://google.com/",
+        footer: "© 2025 Công ty Cổ phần Shipway. All right reserved."
     },
-    title: "Chi tiết đơn hàng",
-    status1: "Tìm tài xế",
-    status2: "Đang vận chuyển",
-    status3: "Hoàn thành",
-    status4: "Đã huỷ",
-    back: "Quay lại",
+
+        
+
     labels: {
         orderId: "Mã đơn hàng",
         pickup: "Điểm nhận hàng",
@@ -24,6 +35,7 @@ const langData = {
         images: "Ảnh hàng hoá"
     },
     values: {
+        statusId: 1,
         orderId: "ABC001",
         pickup: "Nguyễn Lương Bằng, Phường Liên Chiểu, Thành phố Đà Nẵng",
         delivery: "Khu nhà xưởng số 1, đường số 9, Khu Công nghệ cao Đà Nẵng, Xã Hòa Liên, Huyện Hòa Vang, Thành phố Đà Nẵng",
@@ -31,13 +43,8 @@ const langData = {
         size: "dài x rộng x cao",
         weight: "Chủ hàng tự ước tính",
         driver: "Chưa có",
-        driverPhone: "Chưa có",
+        driverPhone: "0123456789",
         cost: "Chủ hàng tự ước tính",
-        date: "dd/mm/yyyy",
-        productImage: [
-            "img/box.jpg",
-            "img/box.jpg",
-            "img/box.jpg"
-        ]
+        date: "dd/mm/yyyy"
     }
 };
